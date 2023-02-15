fcc08056ac9e047c4d0c3f4312353701e20c31fb
wildone <max.barrass@gmail.com>
2023-02-15 20:57:59 +1100

    add lint step.

--- test.sh
+++ test.sh
@@ -3,4 +3,6 @@ yum install -y python3
 pip3 install --ignore-installed PyYAML
 pip3 install --upgrade pip
 pip3 install testinfra molecule docker molecule-docker yamllint flake8 ansible ansible-lint
+yamllint .
+ansible-lint .
 molecule --debug test
